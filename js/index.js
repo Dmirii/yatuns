3ee5005cfa55420dc1e967d03fec0f861c8d393a
Dima A <dimaa@dimaa.ru>
2020-07-28 18:27:56 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
 const temp = document.querySelector('.temp');
 
+const videoPlayer = document.querySelector('.video-player');
+const videoButtonPlay = document.querySelector('.video-button__play');
+const videoButtonStop = document.querySelector('.video-button__stop');
+const videoTimePassed = document.querySelector('.video-time__passed');
+const videoProgress = document.querySelector('.video-progress');
+const videoTimeTotal = document.querySelector('.video-time__total');
+
+radioPlayerInit();
+musicPlayerInit();
+videoPlayerInit();
 
 //
 // close all tabs
@@ -15,7 +25,6 @@ function deactivationPlayers() {
     playerBlock.forEach(item => item.classList.remove('active'));
     playerBtn.forEach(item => item.classList.remove('active'));
 }
-
 
 //
 // addEvent to btn
@@ -26,8 +35,54 @@ playerBtn.forEach( (btn,index) => {
         playerBlock[index].classList.add('active');
     })
 })
+//
+// 
+const toggleIcon = () => {
+    if(videoPlayer.paused){
+        videoButtonPlay.classList.remove('fa-pause');
+        videoButtonPlay.classList.add('fa-play');
 
+    } else {
+        videoButtonPlay.classList.add('fa-pause');
+        videoButtonPlay.classList.remove('fa-play');
+    }
 
-radioPlayerInit();
-musicPlayerInit();
-videoPlayerInit();
+}
+const togglePlay = () => {
+    if(videoPlayer.paused){
+        videoPlayer.play();
+    } else {
+        videoPlayer.pause();
+    }
+}
+
+const stopPlay = () => {
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+}
+const addZero = n =>  n <10 ? '0' + n : n;
+
+videoPlayer.addEventListener('click', togglePlay);
+videoButtonPlay.addEventListener('click', togglePlay);
+videoPlayer.addEventListener('play',toggleIcon);
+videoPlayer.addEventListener('pause',toggleIcon);
+videoButtonStop.addEventListener('click', stopPlay);
+videoPlayer.addEventListener('timeupdate', () =>{
+    videoProgress.value = (currentTime/duration) * 100;
+    const currentTime = videoPlayer.currentTime;
+    const duration = videoPlayer.duration;
+    let minuetPassed = Math.floor(currentTime/60);
+    let secondsPassed = Math.floor(currentTime % 60 );
+    let minuetTotal = Math.floor(duration/60);
+    let secondsTotal = Math.floor(duration % 60 );
+    videoTimePassed.textContent = addZero(minuetPassed) + ':'+ addZero(secondsPassed);
+    videoTimeTotal.textContent = addZero(minuetTotal)+ ':'+ addZero(secondsTotal);
+})
+
+videoProgress.addEventListener('change', () => {
+    const duration = videoPlayer.duration;
+    const value = videoProgress.value;
+    videoPlayer.currentTime = (value * duration) /100;
+
+})
+
