e07b16a9094c4bb120b0d07a30fcc85e42c0395d
Dima A <dimaa@dimaa.ru>
2020-07-30 10:04:02 +0300

    first commit

--- js/index.js
+++ js/index.js
@@ -2,21 +2,43 @@ import {radioPlayerInit} from './radioplayer.js';
 import {videoPlayerInit} from './videoplayer.js';
 import {musicPlayerInit} from './musicplayer.js';
 
+// let lastfm = new LastFM({
+// 	apiKey    : '16bbc846e3cc5f7898ac7dbc426b39d7',
+// 	apiSecret : '0a565dcf333a2c3450ee110ec4a85a62'
+// });
+// const token= 'qKYTbqi6_rXAGS2SHqcP69r-xw0tzDs0';
+// let sk ='';
+// let ts = Math.floor(new Date().getTime()/1000); 
+
+// lastfm.auth.getSession({token: token}, {success: function(data){
+// 	console.log("Привет, "+data.session.name+"!\n\rРад тебя видеть, твою ключ сессии "+data.session.key); // Для видимости
+// 	sk = data.session.key; // В раннее созданную переменную записываем ключ сессии. 
+// }, error: function(code, message){
+// 	if (code == 4)
+// 		console.log("Токен умер. Щелкни снова авторизацию");
+// }});
+
+
+// lastfm.track.scrobble({artist: "Linkin Park", track: "Numb", timestamp: ts}, {key: sk}, {success: function(data){
+// console.log("Всё получилось!");
+// }, error: function(code, message){
+// 	alert("Ничего не получилось...");
+// }});
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
 const temp = document.querySelector('.temp');
 
-const videoPlayer = document.querySelector('.video-player');
-const videoButtonPlay = document.querySelector('.video-button__play');
-const videoButtonStop = document.querySelector('.video-button__stop');
-const videoTimePassed = document.querySelector('.video-time__passed');
-const videoProgress = document.querySelector('.video-progress');
-const videoTimeTotal = document.querySelector('.video-time__total');
+
+
+
+
 
 radioPlayerInit();
 musicPlayerInit();
 videoPlayerInit();
+
+
 
 //
 // close all tabs
@@ -36,53 +58,5 @@ playerBtn.forEach( (btn,index) => {
     })
 })
 //
-// 
-const toggleIcon = () => {
-    if(videoPlayer.paused){
-        videoButtonPlay.classList.remove('fa-pause');
-        videoButtonPlay.classList.add('fa-play');
 
-    } else {
-        videoButtonPlay.classList.add('fa-pause');
-        videoButtonPlay.classList.remove('fa-play');
-    }
-
-}
-const togglePlay = () => {
-    if(videoPlayer.paused){
-        videoPlayer.play();
-    } else {
-        videoPlayer.pause();
-    }
-}
-
-const stopPlay = () => {
-    videoPlayer.pause();
-    videoPlayer.currentTime = 0;
-}
-const addZero = n =>  n <10 ? '0' + n : n;
-
-videoPlayer.addEventListener('click', togglePlay);
-videoButtonPlay.addEventListener('click', togglePlay);
-videoPlayer.addEventListener('play',toggleIcon);
-videoPlayer.addEventListener('pause',toggleIcon);
-videoButtonStop.addEventListener('click', stopPlay);
-videoPlayer.addEventListener('timeupdate', () =>{
-    videoProgress.value = (currentTime/duration) * 100;
-    const currentTime = videoPlayer.currentTime;
-    const duration = videoPlayer.duration;
-    let minuetPassed = Math.floor(currentTime/60);
-    let secondsPassed = Math.floor(currentTime % 60 );
-    let minuetTotal = Math.floor(duration/60);
-    let secondsTotal = Math.floor(duration % 60 );
-    videoTimePassed.textContent = addZero(minuetPassed) + ':'+ addZero(secondsPassed);
-    videoTimeTotal.textContent = addZero(minuetTotal)+ ':'+ addZero(secondsTotal);
-})
-
-videoProgress.addEventListener('change', () => {
-    const duration = videoPlayer.duration;
-    const value = videoProgress.value;
-    videoPlayer.currentTime = (value * duration) /100;
-
-})
 
--- js/radioplayer.js
+++ js/radioplayer.js
@@ -1,4 +1,59 @@
 export const radioPlayerInit = () => {
     console.log('rdaio init');
+    const radio= document.querySelector('.radio');
+    const radioItems= document.querySelectorAll('.radio-item');
+    const radioStop= document.querySelector('.radio-stop');
+    const radioCoverImg= document.querySelector('.radio-cover__img');
+    const radioNavigation= document.querySelector('.radio-navigation');
+    const radioHeader= document.querySelector('.radio-header__big');
+
+    const audio = new Audio();
+    audio.type = 'audio/acc';
+
+    radioStop.disabled = true;
+    const changeIconPlay = () =>{
+        if(audio.paused){
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop');
+        } else{
+            radio.classList.add('play');
+            radioStop.classList.remove('fa-play');
+            radioStop.classList.add('fa-stop');
+        }
+    };
+    const selectItem = elem =>{
+        radioItems.forEach( item => item.classList.remove('select'));
+        elem.classList.add('select');
+    };
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parrent = target.closest('.radio-item');  
+        const title = parrent.querySelector('.radio-name').textContent;
+        const img =parrent.querySelector('.radio-img').src;
+
+        audio.src= target.dataset.radioStantion;
+        selectItem(parrent);
+       
+        radioHeader.textContent = title;
+        radioCoverImg.src=  img;     
+
+        audio.play();
+        radioStop.disabled = false;
+        changeIconPlay();
+    })
+    radioStop.addEventListener('click', () =>{
+        if(audio.paused){
+            audio.play();
+        }else {
+            audio.pause();
+        }
+        changeIconPlay();
+
+    })
+
+
+    
 }
 
